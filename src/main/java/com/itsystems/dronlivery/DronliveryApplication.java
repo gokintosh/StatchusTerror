6a4af3548cceac9aac6ca365aa32dba2c291f5d6
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-03-08 18:43:48 +0100

    initial commit

---
+++ src/main/java/com/itsystems/dronlivery/DronliveryApplication.java
@@ -0,0 +1,13 @@
+package com.itsystems.dronlivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsystems/dronlivery/DronliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsystems.dronlivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
